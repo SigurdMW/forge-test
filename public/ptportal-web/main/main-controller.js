2162c56a26e12b23a9fa695e106d2c4e55cf9a61
Nils Fredrik Kleven <nkl@capraconsulting.no>
2016-02-19 23:18:47 +0100

    List all users på main

--- public/ptportal-web/main/main-controller.js
+++ public/ptportal-web/main/main-controller.js
@@ -1,8 +1,28 @@
-'use strict';
+(function() {
 
-angular.module('gltApp')
-        .controller('MainCtrl', ['$scope','$rootScope',
-            function($scope, $rootScope) {
+    'use strict';
 
+    angular
+        .module('gltApp')
+        .controller('MainCtrl', UserController);  
 
-            }]);
+    function UserController($http) {
+
+        var vm = this;
+        
+        vm.users;
+        vm.error;
+
+        vm.getUsers = function() {
+
+            // This request will hit the index method in the AuthenticateController
+            // on the Laravel side and will return the list of users
+            $http.get('api/v1/users').success(function(users) {
+                vm.users = users;
+            }).error(function(error) {
+                vm.error = error;
+            });
+        }
+    }
+    
+})();
