515a8f21b26ca96d5a0e47c1c9e5c3201cf5657e
Nils Fredrik Kleven <nkl@capraconsulting.no>
2016-02-19 23:32:57 +0100

    dfssdfss

--- public/ptportal-web/main/main-controller.js
+++ public/ptportal-web/main/main-controller.js
@@ -6,25 +6,21 @@
         .module('gltApp')
         .controller('MainCtrl', UserController);  
 
-    function UserController($http, $rootScope) {
+    function UserController($http, $scope ,$rootScope) {
 
     	console.log("UserController / MainCtrl aktiveres!!");
-    	console.log(this);
 
-        var vm = this;
-        
-        vm.users;
-        vm.error;
+       
 
-        vm.getUsers = function() {
+        $scope.getUsers = function() {
         	console.log("getUser blir kalt");
 
             // This request will hit the index method in the AuthenticateController
             // on the Laravel side and will return the list of users
             $http.get('http://188.166.148.21/api/v1/users/'+ $rootScope.cred).success(function(users) {
-                vm.users = users;
+                $scope.users = users;
             }).error(function(error) {
-                vm.error = error;
+                $scope.error = error;
             });
         }
     }
